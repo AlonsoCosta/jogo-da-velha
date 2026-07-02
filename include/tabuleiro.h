08ced440a6fd71238189274e78cd9695d0f1bea2
Alonso Costa <costaalonso40@gmail.com>
2026-07-02 17:03:18 -0300

    Atualizando

--- include/tabuleiro.h
+++ include/tabuleiro.h
@@ -1,18 +1,25 @@
 #ifndef TABULEIRO_H
-#define TABULEIRO_
+#define TABULEIRO_H
 
 #define VAZIO 0
 #define MARCA_O 1
 #define MARCA_X 4
 
-void iniciarTabuleiro();
+#define LINHAS 3
+#define COLUNAS 3
 
-void desenha();
+#define POS_VALIDA 1
+#define POS_INVALIDA -1
+#define POS_OCUPADA -2
 
-int temVencedor();
+void iniciarTabuleiro(int Tabuleiro[LINHAS][COLUNAS]);
 
-void marcaJogada();
+void desenha(int Tabuleiro[LINHAS][COLUNAS]);
 
-void PosicaoValida();
+int temVencedor(int Tabuleiro[LINHAS][COLUNAS]);
+
+void marcaJogada(int Tabuleiro[LINHAS][COLUNAS], int linha, int coluna, int valor);
+
+int posicaoValida(int Tabuleiro[LINHAS][COLUNAS], int linha, int coluna);
 
 #endif
